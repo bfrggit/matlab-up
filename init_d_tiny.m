ecaad7e0a31faa257dfaa24cea24942f16151a0f
Charles Zhu <zhuqiuxiuci@gmail.com>
2015-12-03 15:42:39 -0800

    Added tiny init script

---
+++ init_d_tiny.m
@@ -0,0 +1,21 @@
+% Author: Charles ZHU
+% --
+% This script prepares basic constants for demo generation and simulations
+% This is a tiny case to test slow algorithms
+
+init_p;
+
+% Constants for DS
+N_DS = 8;
+DX_MU = 180;
+DX_SIGMA = 60;
+R_0 = 1500;
+S_0 = 5000;
+DD_M = 80;
+
+% Constants for OP
+N_OP = 4;
+DX_M = 400;
+ER_MU = 500;
+ER_SIGMA = 250;
+ER_MIN = 25;
