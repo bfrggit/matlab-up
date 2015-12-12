4bedd5c6c9eb0e97531bd61e9cbb1ae2559a46df
Charles Zhu <zhuqiuxiuci@gmail.com>
2015-12-11 17:15:27 -0800

    Added stats

--- big_op_number_batch.m
+++ big_op_number_batch.m
@@ -34,15 +34,19 @@ number_of_op = (10:10:200)';
 dxs_m = LENGTH./ number_of_op;
 nm_op = size(number_of_op, 1);
 loop_n = N_LOOP * nm_op;
-%reward_total = zeros(nm_op, 3);
-%time_running = zeros(nm_op, 3);
-reward_total = zeros(nm_op, 2);
-time_running = zeros(nm_op, 2);
+reward_total = zeros(nm_op, 3);
+time_running = zeros(nm_op, 3);
+rate_total = zeros(nm_op, 9);
+rate_all_total = zeros(nm_op, 3);
+length_task = zeros(nm_op, 3);
 
 tic
 for j = 1:nm_op
-    rw1_total = 0.0; rw2_total = 0.0; %rw3_total = 0.0;
-    et_plan1 = 0.0; et_plan2 = 0.0; %et_plan3 = 0.0;
+    reward_acc = zeros(1, 3);
+    time_acc = zeros(1, 3);
+    rate_acc = zeros(3, 6);
+    length_acc = zeros(1, 3);
+    
     for k = 1:N_LOOP
         % Generate demo instances
         v_ds = mk_vec_ds_new(N_DS, DX_MU, DX_SIGMA, R_0, S_M, S_RANGE, DD_M, D_OFFSET, DD_RANGE);
@@ -54,71 +58,103 @@ for j = 1:nm_op
         % ASAP planning
         et = cputime;
         [mat_m, ~] = plan_asap(v_ds, v_op);
-        et_plan1 = et_plan1 + (cputime - et);
+        time_acc(1) = time_acc(1) + (cputime - et);
 
         % Calculate actual upload time
         t_up = vec_t_up(v_ds, v_op, mat_m, T_WAIT);
         v_f = vec_f(v_ds, t_up);
+        t_comp = vec_t_comp(v_ds, v_op, mat_m, T_WAIT); %#ok<*NASGU>
         
-        rw1 = reward(v_ds, v_f);
-        rw1_total = rw1_total + rw1;
+        reward_acc(1) = reward_acc(1) + reward(v_ds, v_f);
+        rate_acc(1, :) = rate_acc(1, :) + rate_new_row(v_ds, t_up);
+        length_acc(1) = length_acc(1) + t_comp(size(t_comp, 1) - 1);
         
         % Algorithm 4 planning
         et = cputime;
         [mat_m, ~] = plan_alg4x(v_ds, v_op, T_WAIT);
-        et_plan2 = et_plan2 + (cputime - et);
+        time_acc(2) = time_acc(2) + (cputime - et);
         
         % Calculate actual upload time
         t_up = vec_t_up(v_ds, v_op, mat_m, T_WAIT);
         v_f = vec_f(v_ds, t_up);
+        t_comp = vec_t_comp(v_ds, v_op, mat_m, T_WAIT);
         
-        rw2 = reward(v_ds, v_f);
-        rw2_total = rw2_total + rw2;
-        
-        % ASAP planning
-        %[cst_m, cst_ls] = plan_asap(v_ds, v_op);
-
-        % GA planning
-        %et = cputime;
-        %[mat_m, ls] = plan_ga(v_ds, v_op, cst_ls, T_WAIT);
-        %et_plan3 = et_plan3 + (cputime - et);
-        %fprintf('\n');
-
-        % Calculate actual upload time
-        %t_up = vec_t_up(v_ds, v_op, mat_m, T_WAIT);
-        %v_f = vec_f(v_ds, t_up);
-        
-        %rw3 = reward(v_ds, v_f);
-        %rw3_total = rw3_total + rw3;
-        fprintf('\n');
+        reward_acc(2) = reward_acc(2) + reward(v_ds, v_f);
+        rate_acc(2, :) = rate_acc(2, :) + rate_new_row(v_ds, t_up);
+        length_acc(2) = length_acc(2) + t_comp(size(t_comp, 1) - 1);
     end
-    reward_total(j, 1) = rw1_total / N_LOOP;
-    reward_total(j, 2) = rw2_total / N_LOOP;
-    %reward_total(j, 3) = rw3_total / N_LOOP;
-    time_running(j, 1) = et_plan1 / N_LOOP;
-    time_running(j, 2) = et_plan2 / N_LOOP;
-    %time_running(j, 3) = et_plan3 / N_LOOP;
+    reward_total(j, :) = reward_acc / N_LOOP;
+    time_running(j, :) = time_acc / N_LOOP;
+    rate_total(j, 1:3) = rate_acc(1, 4:6)./ rate_acc(1, 1:3);
+    rate_total(j, 4:6) = rate_acc(2, 4:6)./ rate_acc(2, 1:3);
+    rate_total(j, 7:9) = rate_acc(3, 4:6)./ rate_acc(3, 1:3);
+    rate_all_total(j, :) = [ ...
+        sum(rate_acc(1, 4:6)) / sum(rate_acc(1, 1:3)), ...
+        sum(rate_acc(2, 4:6)) / sum(rate_acc(2, 1:3)), ...
+        sum(rate_acc(3, 4:6)) / sum(rate_acc(3, 1:3))];
+    length_task(j, :) = length_acc / N_LOOP;
 end
 toc
-%plot(number_of_op, reward_total(:, 1), ...
-%    number_of_op, reward_total(:, 2), '-*', ...
-%    number_of_op, reward_total(:, 3), '-o');
 plot(number_of_op, reward_total(:, 1), ...
-    number_of_op, reward_total(:, 2), '-*');
+    number_of_op, reward_total(:, 2), '-*'); %, ...
+    %number_of_op, reward_total(:, 3), '-o');
 xlabel('Number of upload opportunities');
 ylabel('Weighted overall utility');
-legend('First opportunity', 'Proposed algorithm'); %, 'Genetic algorithm');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
 saveas(gcf, 'fig/big_op_number_reward.fig');
 
 figure;
-%plot(number_of_op, time_running(:, 1), ...
-%    number_of_op, time_running(:, 2), '-*', ...
-%    number_of_op, time_running(:, 3), '-o');
 plot(number_of_op, time_running(:, 1), ...
-    number_of_op, time_running(:, 2), '-*');
+    number_of_op, time_running(:, 2), '-*'); %, ...
+    %number_of_op, time_running(:, 3), '-o');
 xlabel('Number of upload opportunities');
 ylabel('Running time (sec)');
-legend('First opportunity', 'Proposed algorithm'); %, 'Genetic algorithm');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
 saveas(gcf, 'fig/big_op_number_time.fig');
+
+figure;
+plot(number_of_op, rate_total(:, 1), ...
+    number_of_op, rate_total(:, 4), '-*'); %, ...
+    %number_of_op, rate_total(:, 7), '-o');
+xlabel('Number of upload opportunities');
+ylabel('Portion of high priority data chunks uploaded');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
+saveas(gcf, 'fig_2/big_op_number_high.fig');
+
+figure;
+plot(number_of_op, rate_total(:, 2), ...
+    number_of_op, rate_total(:, 5), '-*'); %, ...
+    %number_of_op, rate_total(:, 8), '-o');
+xlabel('Number of upload opportunities');
+ylabel('Portion of medium priority data chunks uploaded');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
+saveas(gcf, 'fig_2/big_op_number_medium.fig');
+
+figure;
+plot(number_of_op, rate_total(:, 3), ...
+    number_of_op, rate_total(:, 6), '-*'); %, ...
+    %number_of_op, rate_total(:, 9), '-o');
+xlabel('Number of upload opportunities');
+ylabel('Portion of low priority data chunks uploaded');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
+saveas(gcf, 'fig_2/big_op_number_low.fig');
+
+figure;
+plot(number_of_op, rate_all_total(:, 1), ...
+    number_of_op, rate_all_total(:, 2), '-*'); %, ...
+    %number_of_op, rate_all_total(:, 3), '-o');
+xlabel('Number of upload opportunities');
+ylabel('Portion of data chunks uploaded');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
+saveas(gcf, 'fig_2/big_op_number_all.fig');
+
+figure;
+plot(number_of_op, length_task(:, 1), ...
+    number_of_op, length_task(:, 2), '-*'); %, ...
+    %number_of_op, length_task(:, 3), '-o');
+xlabel('Number of upload opportunities');
+ylabel('Total time to finish all data collection');
+legend('First opportunity', 'Proposed algorithm', 'Genetic algorithm');
+saveas(gcf, 'fig_2/big_op_number_length.fig');
 
 save('mat/big_op_number.mat')
